b2d4f74144d08fe4333eb95e454a092909585fad
ryancupp <ryancupp89@gmail.com>
2022-08-19 21:21:22 -0600

    Fizzbuzz assignment

---
+++ JavaScript/01-Fizzbuzz/fizz.js
@@ -0,0 +1,14 @@
+for (var i=1; i < 101; i++){
+    if(i%3===0){
+        console.log("Fizz");
+    }
+    if(i%5===0){
+        console.log("Buzz")
+    }
+    if(i%3===0 && i%5===0){
+        console.log("FizzBuzz");
+    }
+    if(i%3 != 0 && i%5 != 0){
+        console.log(i);
+    }
+}   
