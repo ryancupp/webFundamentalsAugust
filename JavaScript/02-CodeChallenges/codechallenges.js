4023937f701b4f496915876d53517415d5edef2e
ryancupp <ryancupp89@gmail.com>
2022-08-21 18:12:49 -0600

    added code challenges

---
+++ JavaScript/02-CodeChallenges/codechallenges.js
@@ -0,0 +1,25 @@
+for (var i=1; i < 20; i += 2){
+    console.log(i);
+}
+
+for (var i=100; i > 0; i--){
+    if(i % 3 ===0){
+        console.log(i);
+    }
+}
+
+for (var i = 4; i >= -3.5; i -= 1.5){
+    console.log(i);
+}
+
+var count=0
+for(var i=1; i < 101; i++){
+    count = count + i;
+}
+console.log(count);
+
+var product=1
+for(var i=1; i < 13; i++){
+    product = product * i;
+}
+console.log(product);
