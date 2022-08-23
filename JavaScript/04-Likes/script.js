f658377921122e9a997d58ac66f2791ee4e44aa0
ryancupp <ryancupp89@gmail.com>
2022-08-22 19:19:14 -0600

    Likes assignment

---
+++ JavaScript/04-Likes/script.js
@@ -0,0 +1,10 @@
+function increaseLikes(elementID){
+    let spanElement = document.querySelector("#"+ elementID);
+
+    let currentCount = spanElement.innerText;
+
+    currentCount++;
+
+    spanElement.innerText = currentCount;
+
+}
